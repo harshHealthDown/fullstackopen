b822ef5e97464109161c9534a3f95504a6266832
shivendra pandey <harshp572004@gmail.com>
2026-03-16 20:33:00 +0530

    version 2

--- part2/Phonebook/src/components/Persons.jsx
+++ part2/Phonebook/src/components/Persons.jsx
@@ -1,7 +1,7 @@
 export const Persons = ({persons,filter,del}) => {
     return(
         <ol>
-            {persons.filter((p)=>p.name.toLowerCase().indexOf(filter.toLowerCase())!==-1).map((p)=>{
+            {persons.filter( p => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1 ).map( p => {
                 return(
                     <li key={p.name}>
                         {p.name} {p.number}
